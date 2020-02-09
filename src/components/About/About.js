f151b6da59c3d03ef58edb4c0cc9ea08a17cd4af
renatajarmova <renatajarmova@gmail.com>
2020-02-09 12:51:33 +0000

    Create assets folder including main svg

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,14 @@
 import React from "react";
+import * as Style from "./About.style";
 
 const About = () => {
   return (
     <>
-      <h2>about me</h2>
-      <p>...</p>
-      {/* image here */}
+      <Style.About>
+        <h2>about me</h2>
+        <p>...</p>
+        {/* image here */}
+      </Style.About>
     </>
   );
 };
